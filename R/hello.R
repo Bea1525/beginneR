b234b874709c71fc7d4c3e5fa6fd628c2436e12b
Bea <maria.flingelli@campus.lmu.de>
2025-07-17 16:48:54 +0200

    Second Function

--- R/hello.R
+++ R/hello.R
@@ -1,5 +1,5 @@
 
-begin_viz <- function() {
+begin_vizguide <- function() {
   "Welcome to the world of R. Explore and Find out more about data visualization on the following links:"
   c("Get from Data to Visualization" = "https://www.data-to-viz.com/",
     "Bar Plots" = "https://r-graph-gallery.com/barplot.html",
@@ -10,6 +10,16 @@ begin_viz <- function() {
     "See More" = "https://r-graph-gallery.com/index.html")
 }
 
-begin_dataname <- function() {
+begin_dataname <- function(path) {
+  if (!file.exists(path)) stop("File does not exist!")
+  if (tolower(tools::file_ext(path)) != "csv") stop("Only .csv files!")
+
+  df <- read.csv(path)
+  base <- basename(path)
+  name <- tolower(substr(base, 1, 3))
+  name <- gsub("[^a-z]", "", name)
+
+  assign(name, df, envir = .GlobalEnv)
+  message(paste0("Dataset saved as'", name))
 
 }
